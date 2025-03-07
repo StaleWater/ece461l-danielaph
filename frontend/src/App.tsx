6715fa50455f24e9d2f709820e125d0e2290af9e
Drew Britt <drewbritt02@gmail.com>
2025-03-06 22:25:32 -0600

    Temp signup page, scaffold out routes and use auth hook

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,36 @@
 import { Container, Typography } from '@mui/material';
+import { Routes, Route, Navigate } from "react-router-dom";
+import AuthProvider from "./contexts/AuthContext";
+import PrivateRoute from "./components/PrivateRoute";
 import Login from './components/Login';
+import Signup from './components/Signup';
+import Projects from './components/Projects';
+
 import './App.css';
 
 function App() {
   return (
     <>
-      <Container>
-        <Typography variant="h3" align="center" gutterBottom>
-          HaaS System
-        </Typography>
-        <Login />
-      </Container>
+      <AuthProvider>
+        <Container>
+          <Typography variant="h3" align="center" gutterBottom>
+            HaaS System
+          </Typography>
+          <Routes>
+            <Route path="/" element={<Navigate to="/login" replace />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route
+              path="/projects"
+              element={
+                <PrivateRoute>
+                  <Projects />
+                </PrivateRoute>
+              }
+            />
+          </Routes>
+        </Container>
+      </AuthProvider>
     </>
   );
 }
--- frontend/src/components/Signup.tsx
+++ frontend/src/components/Signup.tsx
@@ -1,29 +1,27 @@
 import { useState } from 'react';
 import { TextField, Button, Box, Typography } from '@mui/material';
 import { useNavigate } from "react-router-dom";
-import { useAuth } from "../contexts/AuthContext"
 import ErrorMessages from './ErrorMessages';
 import SuccessMessages from './SuccessMessages';
 
-function Login() {
+function Signup() {
   const navigate = useNavigate();
-  const auth = useAuth();
-
   const [userId, setUserId] = useState<string>("");
   const [password, setPassword] = useState<string>("");
   const [errorMessage, setErrorMessage] = useState<string>("");
   const [successMessage, setSuccessMessage] = useState<string>("");
 
-  const handleLogin = () => {
-    // Simulate login logic
-    if (userId === 'admin' && password === 'password') {
-      setSuccessMessage('Login successful!');
-      setErrorMessage('');
-      auth?.login("jwtgoeshere");
-      setTimeout(() => navigate("/projects"), 2000)
+  const handleSignup = () => {
+    // Simulate signup logic
+    if (userId == "" || password == "") {
+        setErrorMessage('User ID and Password fields must not be blank.');
+        setSuccessMessage('');
+    } else if (userId === "admin") {
+        setErrorMessage('User ID already registered.');
+        setSuccessMessage('');
     } else {
-      setErrorMessage('Invalid User ID or Password.');
-      setSuccessMessage('');
+        setSuccessMessage('Registration successful!');
+        setErrorMessage('');
     }
   };
 
@@ -32,7 +30,7 @@ function Login() {
       {errorMessage && <ErrorMessages message={errorMessage} />}
       {successMessage && <SuccessMessages message={successMessage} />}
       <Box sx={{ mt: 3 }}>
-        <Typography variant="h5">Login</Typography>
+        <Typography variant="h5">Signup</Typography>
         <TextField
           label="User ID"
           value={userId}
@@ -48,15 +46,15 @@ function Login() {
           fullWidth
           margin="normal"
         />
-        <Button variant="contained" onClick={handleLogin} sx={{ mt: 2 }}>
-          Login
+        <Button variant="contained" onClick={handleSignup} sx={{ mt: 2 }}>
+          Signup
         </Button>
-        <Button variant="contained" onClick={() => navigate("/signup")} sx={{ mt: 2, marginLeft: "4px" }}>
-          Need an account? Signup
+        <Button variant="contained" onClick={() => navigate("/login")} sx={{ mt: 2, marginLeft: "4px" }}>
+          Have an account? Login
         </Button>
       </Box>
     </>
   );
 }
 
-export default Login;
+export default Signup;
