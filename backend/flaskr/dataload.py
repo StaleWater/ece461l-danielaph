f4444153036b084a5809addf24bde96f64456fb2
Kanonbell <71192643+StaleWater@users.noreply.github.com>
2025-03-11 17:26:13 -0500

    moved server start code to top dir
    heroku references all imports from the top level directory, which is different than how we run the server locally. if the server startup code is top level, import statements will work properly for both heroku and local testing.

--- backend/flaskr/dataload.py
+++ backend/flaskr/dataload.py
@@ -1,10 +1,10 @@
 import pickle
 
 def load():
-    file = open("userdata/data.txt", "rb")
+    file = open("backend/flaskr/userdata/data.txt", "rb")
     users = pickle.loads(file.read())    
     return users
 
 def store(users):
-    file = open("userdata/data.txt", "wb")
+    file = open("backend/flaskr/userdata/data.txt", "wb")
     pickle.dump(users, file)
