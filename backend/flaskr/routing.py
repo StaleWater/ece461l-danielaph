fb9d5fbf1e2b3d19e7af6a9f2e85410e0478cc23
Kanonbell <71192643+StaleWater@users.noreply.github.com>
2025-03-27 19:16:30 -0500

    added a get-user-projects api endpoint

--- backend/flaskr/routing.py
+++ backend/flaskr/routing.py
@@ -1,12 +1,14 @@
 from flask import Flask, redirect, request, url_for, render_template, send_from_directory
 import os
 from backend.usermanager import UserManager
+from backend.hardwareManager import HardwareManager
 from backend.database import Database
 
 app = Flask(__name__, static_folder='../../frontend/dist')
 
 db = Database()
 user_man = UserManager(db)
+hw_man = HardwareManager(db)
 
 # serve frontend files
 @app.route("/", defaults={'somePath': ''})
@@ -21,10 +23,10 @@ def serve_frontend(somePath):
     else:
         return send_from_directory(app.static_folder, 'index.html')
 
-@app.route('/api/login', methods=["POST"])
+@app.route('/api/login')
 def login():
-    username = request.form.get("nm")
-    password = request.form.get("pw")
+    username = request.args.get("uname")
+    password = request.args.get("pw")
 
     try:
         user_man.authenticate(username, password)
@@ -35,14 +37,26 @@ def login():
         return error_msg
 
     
-@app.route('/api/signup', methods=["POST"])
-def create():
-    username = request.form.get("nm")
-    password = request.form.get("pw")
+@app.route('/api/signup')
+def signup():
+    username = request.args.get("uname")
+    password = request.args.get("pw")
 
     try:
         user_man.signup(username, password)
         return "success"
+    
+    except Exception as e:
+        error_msg = e.args[0]
+        return error_msg
+
+
+@app.route('/api/get-user-projects')
+def get_user_projects():
+    username = request.args.get("uname")
+    try:
+        project_ids = user_man.get_user_projects(username)
+        return project_ids
     
     except Exception as e:
         error_msg = e.args[0]
