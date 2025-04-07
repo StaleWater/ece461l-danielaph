eaf29177ee2c298bef613a05521c25e880268f00
Connor <stewart.connorm@gmail.com>
2025-04-07 18:14:17 -0500

    Testing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,63 +1,63 @@
-// import { useState } from "react";
-// import { Container, Typography } from '@mui/material';
-// import { Routes, Route, Navigate } from "react-router-dom";
-// import AuthProvider from "./contexts/AuthContext";
-// import PrivateRoute from "./components/PrivateRoute";
-// import Login from './components/Login';
-// import Signup from './components/Signup';
-// import Projects, {Project} from './components/Projects';
-// import ProjectCreation from './components/ProjectCreation'; 
-// import ProjectJoin from './components/ProjectJoin'; 
-// import './App.css';
+import { useState } from "react";
+import { Container, Typography } from '@mui/material';
+import { Routes, Route, Navigate } from "react-router-dom";
+import AuthProvider from "./contexts/AuthContext";
+import PrivateRoute from "./components/PrivateRoute";
+import Login from './components/Login';
+import Signup from './components/Signup';
+import Projects, {Project} from './components/Projects';
+import ProjectCreation from './components/ProjectCreation'; 
+import ProjectJoin from './components/ProjectJoin'; 
+import './App.css';
 
-// function App() {
-//   const [projects, setProjects] = useState<Project[]>([]);
+function App() {
+  const [projects, setProjects] = useState<Project[]>([]);
 
-//   return (
-//     <>
-//       <AuthProvider>
-//         <Container>
-//           <Typography variant="h3" align="center" gutterBottom>
-//             HaaS System
-//           </Typography>
-//           <Routes>
-//             <Route path="/" element={<Navigate to="/login" replace />} />
-//             <Route path="/login" element={<Login />} />
-//             <Route path="/signup" element={<Signup />} />
-//             <Route
-//               path="/projects"
-//               element={
-//                 <PrivateRoute>
-//                   <Projects projects={projects}/>
-//                 </PrivateRoute>
-//               }
-//             />
-//             <Route
-//               path="/join-project"
-//               element={
-//                 <PrivateRoute>
-//                   <ProjectJoin />
-//                 </PrivateRoute>
-//               }
-//             />
-//             <Route
-//               path="/create-project"
-//               element={
-//                 <PrivateRoute>
-//                   <ProjectCreation onCreateProject={(name, id) => {
-//                     let newProjects = projects;
-//                     let newProject: Project = {name, id};
-//                     newProjects.push(newProject);
-//                     setProjects(newProjects);
-//                   }} />
-//                 </PrivateRoute>
-//               }
-//             />
-//           </Routes>
-//         </Container>
-//       </AuthProvider>
-//     </>
-//   );
-// }
+  return (
+    <>
+      <AuthProvider>
+        <Container>
+          <Typography variant="h3" align="center" gutterBottom>
+            HaaS System
+          </Typography>
+          <Routes>
+            <Route path="/" element={<Navigate to="/login" replace />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route
+              path="/projects"
+              element={
+                <PrivateRoute>
+                  <Projects projects={projects}/>
+                </PrivateRoute>
+              }
+            />
+            <Route
+              path="/join-project"
+              element={
+                <PrivateRoute>
+                  <ProjectJoin />
+                </PrivateRoute>
+              }
+            />
+            <Route
+              path="/create-project"
+              element={
+                <PrivateRoute>
+                  <ProjectCreation onCreateProject={(name, id) => {
+                    let newProjects = projects;
+                    let newProject: Project = {name, id};
+                    newProjects.push(newProject);
+                    setProjects(newProjects);
+                  }} />
+                </PrivateRoute>
+              }
+            />
+          </Routes>
+        </Container>
+      </AuthProvider>
+    </>
+  );
+}
 
-// export default App;
+export default App;
